bcbe92736de89a4b66fab6dce22b87351961dd52
Abdallah Lotfi <abdallahlotfi97@gmail.com>
2026-02-20 22:53:55 +0100

    add header to all pages layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MyComponent } from './my-component/my-component';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
