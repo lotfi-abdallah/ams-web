80567388cb76097b1b956452c847e87e2b449509
Abdallah Lotfi <abdallahlotfi97@gmail.com>
2026-02-20 22:56:04 +0100

    add temporary minimal auth guard

---
+++ frontend/src/guards/auth.guard.ts
@@ -0,0 +1,6 @@
+import { CanActivateFn } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const token = localStorage.getItem('token');
+  return !!token; // Retourne true si le token existe, sinon false
+};
